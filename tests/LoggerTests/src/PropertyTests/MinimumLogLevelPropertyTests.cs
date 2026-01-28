df70282249de5cc8d2ae3b77ce44f23e45d3d95b
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-01-28 08:08:46 +0100

    fix: remove unused using directive in MinimumLogLevelPropertyTests

--- tests/LoggerTests/src/PropertyTests/MinimumLogLevelPropertyTests.cs
+++ tests/LoggerTests/src/PropertyTests/MinimumLogLevelPropertyTests.cs
@@ -4,7 +4,6 @@ using AwesomeLogger;
 using AwesomeAssertions;
 using System.Collections.Generic;
 using R3;
-using System.Linq;
 using System.Threading.Tasks;
 
 public sealed class TheMinimumLogLevelProperty
